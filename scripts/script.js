8196262c1e219b4938b97b7f9cbbee006ca1a316
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-08 20:14:43 +0330

    add the answer of q2

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
+//q2
 'use strict'
+
+function elemCreator(elem,callback)
+{
+    elem=document.createElement(elem)
+    console.log(elem)
+    callback(elem)
+    document.body.appendChild(elem)
+}
+function callback(elem){
+    elem.innerHTML="hello world"
+    elem.style.backgroundColor = 'red'
+    elem.style.color='yellow'
+    elem.style.textAlign="center"
+    elem.style.width='200px'
+    elem.style.height='200px'
+    elem.style.position='absolute'
+    elem.style.top="50%"
+    elem.style.left="50%"
+    elem.style.transform="translate(-50%,-50%)"
+}
+elemCreator('div', callback)
