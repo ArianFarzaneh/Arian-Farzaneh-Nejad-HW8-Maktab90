c87f11e3e385ca61e90a5cf53cd3facd3019338d
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-08 20:09:16 +0330

    add q1 answer

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 'use strict'
+let user={}
+function setterGenerator(key){
+    return function(value){
+        //this inja be object eshare mikonad chon dar .call() user ra pass dadim
+        this[key]=value;
+        return this;
+    }
+}
+let nameSetter=setterGenerator("name")//key ra pass midehad
+nameSetter.call(user,'john')//user ra be onvan object va john ra be onvan value ra pas s  midehad
+console.log(user);
