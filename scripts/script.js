a126f8571f90a87cd458ba4d6afdf764c789e9bb
Arian Farzanehnejad <afn2681@gmail.com>
2023-02-08 20:17:17 +0330

    add answer of q3

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
+//q3
 'use strict'
+//estefade az Closures
+function counterMaker()
+{
+    let counter=0
+    return function(number=0)
+    {
+        counter=counter+number
+        return counter
+    }
+}
+const counter = counterMaker()       
+console.log( counter() ) // 0           
+console.log( counter() ) // 0
+console.log( counter(1) ) // 1
+console.log( counter() ) // 1
+console.log( counter(3) ) // 4
+console.log( counter(7) ) // 11
+console.log( counter(-5) ) // 6
